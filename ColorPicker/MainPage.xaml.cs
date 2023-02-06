3abb6479750cd1bc679a90c7306cb71b5a73e503
EmperorXSama <5alidghoddane@protonmail.com>
2023-02-06 11:53:09 +0100

    Add project files.

---
+++ ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace ColorPicker;
+
+public partial class MainPage : ContentPage
+{
+	 bool israndom;
+	string hexValue;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Slide_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+		if (!israndom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColorValuesToComponents(color);
+        }
+
+    }
+
+	private void SetColorValuesToComponents(Color color)
+	{
+		Container.BackgroundColor= color;
+        btnRandom.BackgroundColor= color;
+		hexValue = color.ToHex();
+        lblHex.Text= "Hex Value: " + hexValue;
+
+    }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+		israndom = true;
+		var random  = new Random();
+		var color = Color.FromRgb(
+			random.Next(0,256),
+			random.Next(0, 256),
+			random.Next(0, 256));
+
+		SetColorValuesToComponents(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+
+		israndom= false;
+    }
+
+    private async  void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		await Clipboard.SetTextAsync(hexValue);
+		var toast = Toast.Make("Color Copied", ToastDuration.Short,12);
+
+		await toast.Show();
+    }
+}
+
